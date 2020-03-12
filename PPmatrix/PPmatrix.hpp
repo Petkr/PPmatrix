6bf210c71ef5e9f3bdd863a7eff84a29460d3c06
Peter Fačko <pfacko1@gmail.com>
2020-03-12 10:13:25 +0100

    Header with all other header included.

---
+++ PPmatrix/PPmatrix.hpp
@@ -0,0 +1,49 @@
+// concepts
+#include "same.hpp"
+#include "nonvoid.hpp"
+#include "iterator.hpp"
+#include "view.hpp"
+#include "matrix_view.hpp"
+
+// algorithms
+#include "accumulate.hpp"
+#include "copy.hpp"
+#include "equal.hpp"
+#include "fill.hpp"
+#include "find.hpp"
+#include "inner_product.hpp"
+#include "max_element.hpp"
+#include "swap_ranges.hpp"
+#include "traverse.hpp"
+#include "zip.hpp"
+
+// matrix containers
+#include "dynamic_matrix.hpp"
+#include "static_matrix.hpp"
+
+// views
+#include "simple_view.hpp"
+#include "simple_matrix_view.hpp"
+#include "augmented_matrix_view.hpp"
+
+// iterator/view adaptors
+#include "unbounded.hpp"
+#include "skip_view.hpp"
+#include "take_view.hpp"
+#include "transform_view.hpp"
+#include "static_view.hpp"
+#include "wrap_view.hpp"
+#include "shift_view.hpp"
+
+// miscellaneous
+#include "number_width.hpp"
+#include "operations.hpp"
+#include "rational.hpp"
+
+// helpers
+#include "default_element.hpp"
+#include "functor.hpp"
+#include "next.hpp"
+#include "prev.hpp"
+#include "compressed_pair.hpp"
+#include "size_t.hpp"
